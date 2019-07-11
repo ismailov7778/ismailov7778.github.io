f71051230a3c88ef4cef8fa36c2ea4a498082bfb
smvrnnmac <smvrnn@MacBook-Air-Erlan.local>
2019-07-11 17:18:46 +0600

    big update + eng version

---
+++ blocks/ru_pages_footer.js
@@ -0,0 +1,17 @@
+
+document.write("<div class=\"footer\">");
+document.write("<div class=\"footleft\">");
+document.write("  <p>КОНТАКТНАЯ ИНФОРМАЦИЯ<br>");
+document.write("Казахстан, г. Нур-Султан, ул. Куйши Дина, 34<br>");
+document.write("Тел.: +7 (7172) 369867, +7 (7172) 369868<br>");
+document.write("<a href=\"mailto:info@miras-astana.kz\">info@miras-astana.kz</a></p>");
+document.write("</div>");
+document.write("<div class=\"footright\">");
+document.write("<div class=\"l1\"><a href=\"https://www.ibo.org/\"><img src=\"../../images/sources/ib.png\" alt=\"\"></a></div>");
+document.write("<div class=\"l2\"><a href=\"https://www.cois.org/\"><img src=\"../../images/sources/cis.png\" alt=\"\"></a></div>");
+document.write("<div class=\"l3\"><a href=\"https://www.ecis.org/\"><img src=\"../../images/sources/ecis.png\" alt=\"\"></a></div>");
+document.write("<div class=\"l4\"><a href=\"https://www.neasc.org/\"><img src=\"../../images/sources/neasc.png\" alt=\"\"></a></div>");
+document.write("<div class=\"l5\"><a href=\"http://ibsa.su/\"><img src=\"../../images/sources/asmb.jpg\" alt=\"\"></a></div>");
+document.write("<div class=\"l6\"><a href=\"https://en.unesco.org/\"><img src=\"../../images/sources/unesco.png\" alt=\"\"></a></div>");
+document.write("</div>");
+document.write("</div>");
